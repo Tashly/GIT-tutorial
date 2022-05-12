ddc8e4d6124012e60f5178e82c6ac45845a5c556
Tashly <tashabakari@gmail.com>
2022-05-12 11:41:39 +0300

    commit

--- 2. FlightCabinCrewTechlogSp.sql
+++
@@ -1,5 +0,0 @@
-SELECT  d.TechLogNo, d.ScheduledDeparture, d.ActualDeparture, f.AircraftReg, f.FlightDate,d.IATACode
-FROM   dbo.FlightDelays d INNER JOIN
-                         dbo.Flight f ON d.TechLogNo = f.TechLogNo
-
-select IA
